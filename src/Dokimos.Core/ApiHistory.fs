dd3a6a95aa67692dc5bbcc71ca8c39e98e067467
Kevin Miller <kemiller2002@gmail.com>
2026-09-26 09:21:59 +0200

    Add public API history evidence: src/Dokimos.Core/ApiHistory.fs

---
+++ src/Dokimos.Core/ApiHistory.fs
@@ -0,0 +1,35 @@
+namespace Dokimos.Core
+
+type ApiSymbol =
+    { Project: string
+      QualifiedName: string
+      Signature: string }
+
+type ApiChange =
+    | Added of ApiSymbol
+    | Removed of ApiSymbol
+    | SignatureChanged of before: ApiSymbol * after: ApiSymbol
+
+type ApiHistory =
+    { Added: int
+      Removed: int
+      Changed: int
+      Changes: ApiChange list }
+
+module Api =
+    let compare before after =
+        let byName symbols = symbols |> List.map (fun x -> (x.Project,x.QualifiedName),x) |> Map.ofList
+        let b = byName before
+        let a = byName after
+        let keys = Set.union (b |> Map.keySet) (a |> Map.keySet)
+        let changes =
+            [ for key in keys do
+                match Map.tryFind key b, Map.tryFind key a with
+                | None, Some current -> Added current
+                | Some previous, None -> Removed previous
+                | Some previous, Some current when previous.Signature <> current.Signature -> SignatureChanged(previous,current)
+                | _ -> () ]
+        { Added = changes |> List.sumBy (function Added _ -> 1 | _ -> 0)
+          Removed = changes |> List.sumBy (function Removed _ -> 1 | _ -> 0)
+          Changed = changes |> List.sumBy (function SignatureChanged _ -> 1 | _ -> 0)
+          Changes = changes }
